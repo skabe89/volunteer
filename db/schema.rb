352a8c6be2da16fa6bd2a591c139e50b6ed14085
Sean Abe <skabe89@gmail.com>
2021-08-12 14:07:10 -0400

    connected to postgres db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_02_02_214833) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "communities", force: :cascade do |t|
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
